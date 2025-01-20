700ef464b7f1288f9079bcf180478a413698ffe2
seolman <tjfehdgns@gmail.com>
2025-01-20 09:15:53 +0900

    Add: glfw blank window

--- opengl/src/main.cpp
+++ opengl/src/main.cpp
@@ -1,6 +1,34 @@
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 int main() {
-  std::cout << "Computer Graphics" << std::endl;
+  if (!glfwInit()) {
+    std::cerr << "Failed to initialize GLFW" << std::endl;
+    return -1;
+  }
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow *window =
+      glfwCreateWindow(800, 600, "Simple GLFW Window", nullptr, nullptr);
+  if (!window) {
+    std::cerr << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+
+    glfwSwapBuffers(window);
+  }
+  
+  glfwDestroyWindow(window);
+  glfwTerminate();
+
   return 0;
 }
